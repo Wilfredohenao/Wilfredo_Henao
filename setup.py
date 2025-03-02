11c5734de6b1ca0cd3a47caec5d6e6c4a6b903d4
Wilfredohenao <wilfredo.henao@est.iudigital.edu.co>
2025-03-02 19:35:49 +0000

    carga inicial

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="wilfredo Henao",
+    author_email="wilfredo.henao@est.iudigital.edu.co",
+    description="Actividad programacion oo",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "requests",
+        "matplotlib"
+    ]
+    
+    
+)
