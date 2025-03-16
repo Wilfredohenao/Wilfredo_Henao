7e4987d1510872dcda6cd07cf3e0a6b60ff09b99
Wilfredohenao <wilfredo.henao@est.iudigital.edu.co>
2025-03-16 21:16:12 +0000

    error

--- setup (1).py
+++ setup (1).py
@@ -8,7 +8,7 @@ setup(
     description="actividad programacion oob",
     py_modules=["actividad_1","actividad_2"],
     install_requires=[
-        "kagglehub[pandas-datasets]>=0.3.8",
+        "kagglehub;[pandas-datasets]>=0.3.0",
         "matplotlib>=3.5.0",
         "seaborn>=0.11.2",
         "pandas",
