1fa4ee719389a800e4d68706b8c5ba13284d83b7
Wilfredohenao <wilfredo.henao@est.iudigital.edu.co>
2025-04-05 21:06:37 +0000

    arreglo final ojala sirva

--- src/pad/actividad_3.py
+++ src/pad/actividad_3.py
@@ -18,7 +18,7 @@ class actividad3:
         self.ruta_act2 = "{}/src/pad/actividad_3/".format(self.ruta_raiz)
         datos = {
             "n_punto": [1,2,3,4,5,6,7,8,9,10,11,12],
-            "detalle":["Crea un DataFrame frutas que luzca así","Crea un DataFrame ventas_frutas que coincida con el diagrama","Crea una variable utensilios con una Serie que tenga el siguiente aspecto:","Descarga el dataset 'wine review' desde Kaggle y cárgalo en un DataFrame llamado review, tal y como se muestra en la figura.","Visualiza las primeras filas del DataFrame","","","","","","",""],
+            "detalle":["Crea un DataFrame frutas que luzca así","Crea un DataFrame ventas_frutas que coincida con el diagrama","Crea una variable utensilios con una Serie que tenga el siguiente aspecto:","Descarga el dataset 'wine review' desde Kaggle y cárgalo en un DataFrame llamado review, tal y como se muestra en la figura.","Visualiza las primeras filas del DataFrame","Utiliza el método .info() para averiguar cuántas entradas hay. ¿Cuántas encontraste?","¿Cuál es el precio promedio?","¿Cuál es el precio más alto pagado?","Crea un DataFrame con todos los vinos de california","Utiliza idxmax() para encontrar el índice del vino con el precio más alto y luego utiliza loc para obtener toda la información de ese vino específico","",""],
             "resultado":[0,0,0,0,0,0,0,0,0,0,0,0],
             
         }
@@ -63,44 +63,69 @@ class actividad3:
         df_5 = df_5.head()
         df_5.to_csv(r"src/pad/actividad_3/punto_5.csv")
         self.df.loc[4,"resultado"] = "src/pad/actividad_3/punto_5.csv"
-        print("punto_5") 
-       
+        print("punto_5")     
     def punto_6(self):
-        df = pd.read_csv(self.ruta_act2, "winemag-data_first150k.csv.zip")
-        df_6 = df.info()
-        df_6.to_csv()
-        self.df.loc[5,"resultado"] =+1
+        ruta_winemag = os.path.join(self.ruta_act2, "winemag-data_first150k.csv.zip")
+        df_6 = pd.read_csv(ruta_winemag)
+        df_6.info()
+        print(df_6.info())
+        df_6.to_csv(r"src/pad/actividad_3/punto_6.csv")
+        self.df.loc[5,"resultado"] = "src/pad/actividad_3/punto_6.csv"
         print("punto_6") 
     def punto_7(self):
-        df_7= pd.DataFrame()
-        ruta_winemag = os.path.join(self.ruta_act2, "winemag-data_first150k.csv.zip")
-        df = pd.read_csv()
-        df_7 = df.mean()
-        df_7.to_csv()
-        self.df.loc[5,"resultado"] = +0
+        ruta_winemag = os.path.join (self.ruta_act2 , "winemag-data_first150k.csv.zip")
+        df_7 = pd.read_csv(ruta_winemag)
+        df_7['price'].mean()
+        df_7.to_csv(r"src/pad/actividad_3/punto_7.csv")
+        self.df.loc[6,"resultado"] = "src/pad/actividad_3/punto_7.csv"
         print("punto_7") 
     def punto_8(self):
-        df_8 = pd.DataFrame()
-        precio_maximo = df['precio'].max()
-        print(precio_maximo)
-        ruta_winemag = os.path.join(self.ruta_act2, "winemag-data_first150k.csv.zip")
-        df = pd.read_csv()  
-        df_8.to_csv()
-        self.df.loc[7,"resultado"] = +1
+        ruta_winemag = os.path.join (self.ruta_act2  , "winemag-data_first150k.csv.zip")
+        df_8 = pd.read_csv(ruta_winemag)
+        df_8 ['price'].max()
+        df_8.to_csv(r"src/pad/actividad_3/punto_8.csv")
+        self.df.loc[7,"resultado"] = "src/pad/actividad_3/punto_8.csv"
         print("punto_8") 
     def punto_9(self):
-        self.df.loc[8,"resultado"] = len(self.df)+8
+        ruta_winemag = os.path.join(self.ruta_act2, "winemag-data_first150k.csv.zip")
+        df_9 = pd.read_csv(ruta_winemag)
+        df_california = df_9[df_9['province']=="California"]
+        df_california.to_csv(r"src/pad/actividad_3/punto_9.csv")
+        df_california = df_california.head() 
+        df_california["description"] = df_california["description"].str.slice(0, 20) + "..."
+        df_california["variety"] = df_california["variety"].str.slice(0, 10) + "..."
+        df_california["designation"] = df_california["designation"].astype(str).str.slice(0, 10) + "..." 
+        self.df.loc[8,"resultado"] = "src/pad/actividad_3/punto_9.csv"        
         print("punto_9") 
     def punto_10(self):
-        
-        self.df.loc[9,"resultado"] = len(self.df)+9
+        df_10 = pd.read_csv("src/pad/actividad_3/punto_9.csv")
+        df_california = df_10[df_10['province'] == "California"]
+        df_california.to_csv(r"src/pad/actividad_3/punto_10.csv")
+        idx_mas_caro = df_california['price'].idxmax()
+        vino_mas_caro = df_california.loc[idx_mas_caro]
+        self.df.loc[9, "resultado"] = "src/pad/actividad_3/punto_10.csv" 
         print("punto_10") 
+        print("El vino más caro de California es:")
+        print(vino_mas_caro.to_string()) 
     def punto_11(self):
-        self.df.loc[10,"resultado"] = len(self.df)+10
-        print("punto_11") 
+        ruta_winemag = os.path.join(self.ruta_act2 , "winemag-data_first150k.csv.zip")
+        df_11 = pd.read_csv(ruta_winemag)
+        df_ca = df_11[df_11['province'] == 'California']
+        varieties = df_ca['variety'].value_counts()
+        print(varieties.head(5))
+        df_11.to_csv(r"src/pad/actividad_3/punto_11.csv")
+        self.df.loc[10,"resultado"] = "src/pad/actividad_3/punto_11.csv"
+        print("punto_11")
+        
     def punto_12(self,num=100):
-        self.df.loc[11,"resultado"] = len(self.df)+11
-        print("punto_12") 
+        ruta_winemag = os.path.join(self.ruta_act2 , "winemag-data_first150k.csv.zip")
+        df_12 = pd.read_csv(ruta_winemag)
+        df_ca = df_12[df_12['province'] == 'California']
+        variety_counts = df_ca['variety'].value_counts()
+        top_10_varieties = variety_counts.head(10)
+        df_12.to_csv(r"src/pad/actividad_3/punto_12.csv")
+        self.df.loc[11,"resultado"] = "src/pad/actividad_3/punto_12.csv"
+        print(top_10_varieties)
     
     def ejecutar(self):
       self.punto_1()     
@@ -108,14 +133,14 @@ class actividad3:
       self.punto_3() 
       self.punto_4() 
       self.punto_5() 
-     #self.punto_6() 
-     #self.punto_7() 
-     #self.punto_8() 
-      #  self.punto_9() 
-      #  self.punto_10()
-      #  self.punto_11()
-      #  self.punto_12()
-       # self.df.to_csv("actividad3.csv")
+      self.punto_6() 
+      self.punto_7() 
+      self.punto_8() 
+      self.punto_9() 
+      self.punto_10()
+      self.punto_11()
+      self.punto_12()
+      self.df.to_csv("actividad3.csv")
 
     
       self.df.to_csv("actividad3.csv", index=False)
